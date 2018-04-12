8a61d84ed9fa1d5046cf4ec9c1da449a5000c203
Khomko-Dima <Khomko-Dima@users.noreply.github.com>
2018-04-12 17:43:56 +0300

    Commit Smitler

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(function() {
+	// Custom JS
+	$(document).ready(function() {
+  	$("a.scrollto").click(function () {
+    elementClick = $(this).attr("href")
+    destination = $(elementClick).offset().top;
+    $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+    return false;
+  });
+});
+	new WOW().init();
+});
