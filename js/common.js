3f3b8b8c043d96c81d6a0b9d8ea346b40b70077b
Khomko-Dima <Khomko-Dima@users.noreply.github.com>
2018-10-26 00:46:37 +0300

    Вернул портфолио

--- js/common.js
+++ js/common.js
@@ -1,7 +1,15 @@
 $(function() {
-	$(function(){
-		$('#Container').mixItUp();
+	// Custom JS
+	$(document).ready(function() {
+  		$("a.scrollto").click(function () {
+    	elementClick = $(this).attr("href");
+    	destination = $(elementClick).offset().top;
+    	$("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+    	return false;
+  		});
 	});
-	
-	// $('.s-plans-box').equalHeights();
+	$(document.body).ready(function() {
+		$(".preloader").fadeOut("slow");
+	});
+	new WOW().init();
 });
