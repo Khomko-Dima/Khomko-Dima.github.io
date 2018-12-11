6e0df2420b8e61175326f90632d4c3350e25a5da
Khomko-Dima <Khomko-Dima@users.noreply.github.com>
2018-12-11 16:13:30 +0300

    Логотипы

--- seti/js/common.js
+++ seti/js/common.js
@@ -5,6 +5,10 @@ $(function() {
         navbar: {
             title: '<p><span>Электромонтаж</span> Гродно</p>'
         },
+        onClick: {
+            close: true,
+            preventDefault: false,
+        },
       });
 
     var api1 = $('#my-menu').data('mmenu');
@@ -13,9 +17,36 @@ $(function() {
     }).bind('close:finish', function() {
         $('.hamburger').removeClass('is-active');
     });
+    // $('.mm-listitem__text').click(function() {
+    //     $('.hamburger').removeClass('is-active');
+    //     $('#my-menu').removeClass('mm-menu_opened');
+    // });
 
-
- 
+    $('.partners-galery').owlCarousel({
+        loop: true,
+        smartSpeed: 700,
+        dots: false,
+        nav: true,
+        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+        responsiveClass: true,
+        responsive:{
+        0:{
+            items:1
+        },
+        500:{
+            items:2
+        },
+        576:{
+            items:3
+        },
+        768:{
+            items:4
+        },
+        992:{
+            items:5
+        }
+        }
+    });
 
 	$(document.body).ready(function() {
 		$(".preloader").fadeOut("slow");
